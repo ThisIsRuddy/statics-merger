a0e25de86d9759e62543f720b89ec92c48aa12c6
Dan Rudd <thisisruddy@gmail.com>
2021-07-14 15:10:47 +0100

    Added missing abstracted function stubs

--- src/StaticsMergerPlugin.php
+++ src/StaticsMergerPlugin.php
@@ -434,4 +434,12 @@ class StaticsMergerPlugin implements PluginInterface, EventSubscriberInterface
             ucwords($mappingDir)
         );
     }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
 }
